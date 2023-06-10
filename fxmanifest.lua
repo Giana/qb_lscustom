f98517f384efdd86531082ed78f4487538be8fd9
Giana <42662834+Giana@users.noreply.github.com>
2023-06-10 13:46:23 -0500

    Correct version

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ game 'gta5'
 
 description 'ESX LS Customs (Converted to QB LS Customs by Giana - github.com/Giana)'
 lua54 'yes'
-version '1.0'
+version '1.0.0'
 
 shared_scripts {
     '@qb-core/shared/locale.lua',
